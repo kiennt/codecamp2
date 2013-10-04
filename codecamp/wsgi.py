22b09e41231e98732308253a3d4ac881d3c1ac1e
kiennt <trungkien2288@gmail.com>
2013-10-05 01:42:59 +0700

    Serving static files on heroku

--- codecamp/wsgi.py
+++ codecamp/wsgi.py
@@ -24,8 +24,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
